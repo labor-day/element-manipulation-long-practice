a67c8d90fbcee1d7dc5d314fd184a915c98e09a7
labor-day <shaffer.scott@yahoo.com>
2022-08-04 21:53:47 -0600

    fixed countdown

--- my-profile.js
+++ my-profile.js
@@ -84,7 +84,7 @@ const countDown = () => {
   let now = new Date().getTime();
   let distance = birthday - now;
 
-  var days = distance % (1000 * 60 * 60 * 24);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
